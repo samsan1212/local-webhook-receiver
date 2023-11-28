970e6850ce40fb8d93e205d8481b604e6fb6f67c
Sam TS Chan <samchan148@gmail.com>
2023-11-28 13:23:18 +0800

    feat: add write log file feature

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,36 @@ import { convertRequestBody } from "~/utils/convert_request_body.ts";
 
 const handler = async (req: Request): Promise<Response> => {
   logger.info(`Request received: ${req.url}`);
+  let file: Deno.FsFile | undefined;
+  if (!Deno.env.get("NO_LOG_FILE")) {
+    const logFileUrl = new URL("../server.log", import.meta.url).pathname;
+    file = await Deno.open(logFileUrl, {
+      create: true,
+      write: true,
+      append: true,
+    });
+  }
 
   const headers = Object.fromEntries(req.headers.entries());
-  logger.info(`Request headers:\n\n${JSON.stringify(headers, null, 2)}\n`);
+  const headerMessage = `Request headers:\n\n${
+    JSON.stringify(headers, null, 2)
+  }\n`;
+  logger.info(headerMessage);
+
+  await file?.write(new TextEncoder().encode(headerMessage)).catch((err) => {
+    logger.error("Fail to write logfile: %s", err.message);
+  });
 
   if (req.body) {
     const body = await convertRequestBody(req);
-    logger.info(`Request body:\n\n${JSON.stringify(body, null, 2)}\n`);
+    const bodyMessage = `Request body:\n\n${JSON.stringify(body, null, 2)}\n`;
+    logger.info(bodyMessage);
+    await file?.write(new TextEncoder().encode(bodyMessage)).catch((err) => {
+      logger.error("Fail to write logfile: %s", err.message);
+    });
   }
+
+  file?.close();
 
   return new Response(JSON.stringify({ message: "ok" }), {
     status: 200,
